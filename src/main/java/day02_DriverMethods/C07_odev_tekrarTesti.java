ed8467a81c267265bb6bddc1ec2a09d44a1f7785
Nevzat <nevzatcelik151907@gmail.com>
2022-10-05 16:11:27 +0200

    selenium

--- src/main/java/day02_DriverMethods/C07_odev_tekrarTesti.java
+++ src/main/java/day02_DriverMethods/C07_odev_tekrarTesti.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C07_odev_tekrarTesti {
+    // Her zamanki gibi main methodumuzu calistiralim!!!
     public static void main(String[] args) throws InterruptedException {
         /*
-1. Yeni bir class olusturun (TekrarTesti)
-2. Youtube web sayfasına gidin ve sayfa başlığının “youtube” olup olmadığını doğrulayın (verify), eğer değilse doğru başlığı(Actual Title) konsolda yazdirin.
+1. Yeni bir class olusturun (TekrarTesti)---- (Clasimizi olusturduk...)
+2. Youtube web sayfasına gidin ve sayfa başlığının “youtube” olup olmadığını doğrulayın (verify), eğer değilse
+doğru başlığı(Actual Title) konsolda yazdirin.
 3. Sayfa URL’sinin “youtube” içerip içermediğini (contains) doğrulayın, içermiyorsa doğru URL’yi yazdırın.
 4. Daha sonra Amazon sayfasina gidin https://www.amazon.com/
 5. Youtube sayfasina geri donun
@@ -19,45 +21,76 @@ public class C07_odev_tekrarTesti {
 11.Sayfayi kapatin
          */
 
+        // Selenium da System.SetProperty ile webdriver ve chrome driverimizin konumu burada dosyayi oradan getirmek
+        // icin kullaniriz
         System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
+        WebDriver driver=new ChromeDriver(); // Chrome driverimizi web driver methodundan buraya method olarak olustuyoruz
 
-        driver.get("https://www.youtube.com");
+       // 2. Youtube web sayfasına gidin
+        driver.get("https://www.youtube.com"); //
 
-        if (driver.getCurrentUrl().contains("youtube")) {
-            System.out.println("This url contains : "+driver.getCurrentUrl() );
+        /* Youtube” olup olmadığını doğrulayın (verify), eğer değilse
+            doğru başlığı(Actual Title) konsolda yazdirin.*/
+
+        if (driver.getTitle().contains("youtube")) {
+            System.out.println("This title contains of : "+ driver.getTitle() );
         }else {
-            System.out.println(driver.getCurrentUrl());
+            System.out.println("Actual title :"+ driver.getTitle());
         }
-        Thread.sleep(3000);
+        Thread.sleep(2000); // Sayfalar arasi gecisleri gormek icin aralara 2 saniye bekleme suresi
 
-        driver.navigate().to("https://www.amazon.com");
-        Thread.sleep(3000);
+        // 3. Sayfa URL’sinin “youtube” içerip içermediğini (contains) doğrulayın, içermiyorsa doğru URL’yi yazdırın.
+        if (driver.getCurrentUrl().contains("youtube")) {
+            System.out.println("This Url contains of :"+driver.getCurrentUrl());
+        }else {
+            System.out.println("Actual URL: "+driver.getCurrentUrl());
+        }
 
-        driver.navigate().back();
-        Thread.sleep(3000);
-
-        driver.navigate().refresh();
+        //4. Daha sonra Amazon sayfasina gidin https://www.amazon.com/
+        driver.navigate().to("https://www.amazon.com/");
+        Thread.sleep(2000);
+        //5. Youtube sayfasina geri donun
+        driver.navigate().back(); // driverimizi navigate ile back yaparak 1 geri gitme islemi yapiyoruz
         Thread.sleep(2000);
 
-        driver.navigate().back();
+        // 6. Sayfayi yenileyin
+        driver.navigate().refresh(); // driverimizi navigate ile refresh yaparak sayfayi yeniliyoruz
         Thread.sleep(2000);
 
-        driver.manage().window().fullscreen();
-        Thread.sleep(3000);
+        //7. Amazon sayfasina donun
+        driver.navigate().forward(); // driverimizi forward ile bir ileri sayfaya gitmek icin kullaniyoruz
+        Thread.sleep(2000);
+        /* eger ki bir geri islemi gerceklestirdiysek (back ile) sonrasinda forward ile bir ileri giderek bir onceki
+        sayfaya ulasmis oluruz.Yoksa baska bir sayfaya driver.navigate.to(url:"https://") ile gideriz. */
+
+        // 8. Sayfayi tamsayfa yapin
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        //9. Ardından sayfa başlığının “Amazon” içerip içermediğini (contains) doğrulayın,
+        // Yoksa doğru başlığı(Actual Title) yazdırın.
         if (driver.getTitle().contains("Amazon")) {
-            System.out.println(driver.getTitle());
+            System.out.println("This site contains of :"+ driver.getTitle());
         } else {
             System.out.println("Actual title :"+driver.getTitle());
         }
 
+        // 10.Sayfa URL’sinin https://www.amazon.com/ olup olmadığını doğrulayın, degilse doğru URL’yi yazdırın
         if (driver.getCurrentUrl().contains("https://www.amazon.com/")) {
             System.out.println("Sayfa URL: " +driver.getCurrentUrl());
         } else {
-            driver.getCurrentUrl();
+            System.out.println("Actual URL : "+driver.getCurrentUrl());;
         }
 
+        //11.Sayfayi kapatin
         driver.close();
+        /* Iki sekilde driverimizi kapatabiliriz;
+        1-driver.close(): close() yöntemi, o anda odaklanılan pencereyi kapatır ve geçerli
+        pencere tek açık pencere ise sürücüden çıkar.
+        2-driver.quit(): Quit() yöntemi, ilişkili tüm pencereleri kapatarak sürücüden çıkar.
+
+         */
+
 
     }
 }
--- src/main/java/day02_DriverMethods/C08_HomeWork.java
+++ src/main/java/day02_DriverMethods/C08_HomeWork.java
@@ -6,27 +6,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class C08_HomeWork {
     public static void main(String[] args) throws InterruptedException {
         /*
-        1.Yeni bir class olusturalim (Homework)
-2.ChromeDriver kullanarak, facebook sayfasina gidin ve sayfa basliginin (title) “facebook” oldugunu dogrulayin (verify), degilse dogru basligi yazdirin.
-3.Sayfa URL’inin “facebook” kelimesi icerdigini dogrulayin, icermiyorsa “actual” URL’i yazdirin.
-4.https://www.walmart.com/ sayfasina gidin.
-5. Sayfa basliginin “Walmart.com” icerdigini dogrulayin.
-6. Tekrar “facebook” sayfasina donun
-7. Sayfayi yenileyin
-8. Sayfayi tam sayfa (maximize) yapin
-9.Browser’i kapatin
+    1.Yeni bir class olusturalim (Homework)
+    2.ChromeDriver kullanarak, facebook sayfasina gidin ve sayfa basliginin (title) “facebook” oldugunu dogrulayin
+        (verify), degilse dogru basligi yazdirin.
+    3.Sayfa URL’inin “facebook” kelimesi icerdigini dogrulayin, icermiyorsa “actual” URL’i yazdirin.
+    4.https://www.walmart.com/ sayfasina gidin.
+    5. Sayfa basliginin “Walmart.com” icerdigini dogrulayin.
+    6. Tekrar “facebook” sayfasina donun
+    7. Sayfayi yenileyin
+    8. Sayfayi tam sayfa (maximize) yapin
+    9.Browser’i kapatin
          */
         System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
 
-        driver.get("https:/www.facebook.com");
+        driver.get("https://www.facebook.com");
+
 
         if (driver.getTitle().contains("facebook")) {
-            System.out.println("Facebook title = "+driver.getTitle()+" ");
+            System.out.println("Facebook title = "+driver.getTitle());
         } else {
             System.out.println("Facebook title is: "+driver.getTitle());
         }
-        Thread.sleep(2000);
+
 
         if (driver.getCurrentUrl().contains("facebook")){
             System.out.println("Facebook Url contains 'facebook'");
@@ -38,7 +40,7 @@ public class C08_HomeWork {
         Thread.sleep(2000);
 
         if (driver.getTitle().contains("Walmart.com")) {
-            System.out.println("https://www.walmart.com/ contains "+"Walmart.com");
+            System.out.println("This site contains "+"Walmart.com");
         } else {
             System.out.println(driver.getTitle());
         }
